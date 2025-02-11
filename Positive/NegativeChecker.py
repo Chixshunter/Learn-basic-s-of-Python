7fbb98f6a2b7e6d34708ee6b99e2d7d33c584427
Tsenre101 <tsenredreo@gmail.com>
2025-02-11 23:07:42 +0800

    Learn basic's of Python

---
+++ Positive/NegativeChecker.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print(f"This number '{num}' is positive")
+elif num < 0:
+    print(f"This number {num} is negative")
+else:
+    print(f"This number {num} is 0")       
